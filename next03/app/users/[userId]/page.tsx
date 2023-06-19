5a73ed73e7aaf330b44c434841e4424d433a13a1
Joao Victor Dantas Beyer <joaovictordantasbeyer@MBP-von-Joao.fritz.box>
2023-06-19 20:14:30 +0200

    updated

--- next03/app/users/[userId]/page.tsx
+++ next03/app/users/[userId]/page.tsx
@@ -1,5 +1,6 @@
 import getUser from "@/lib /getUser";
 import getUserPost from "@/lib /getUserPost";
+import {Suspense} from "react"
 
 type Params = {
   params: {
@@ -11,5 +12,17 @@ export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId);
   const userPostsData: Promise<Post[]> = getUserPost(userId);
 
-  return;
+  //start in parallel
+  //const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  const user = await userData
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+      <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
 }
